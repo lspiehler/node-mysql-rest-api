631b1c66922da69fe5a8bad37184ae9b16511d48
Lyas Spiehler <lyas.spiehler@sapphirehealth.org>
2024-03-15 11:30:52 -0500

    first commit

---
+++ routes/insert.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var router = express.Router();
+var mysqllib = require('../lib/mysql');
+
+router.put('/', function(req, res, next) {
+    let mysql = new mysqllib();
+    let connection = mysql.connect({
+        host: req.body.mysql.host,
+        user: req.body.mysql.user,
+        password: req.body.mysql.password,
+        database: req.body.mysql.database
+    });
+    //console.log(req.body.values);
+    let query = connection.query(req.body.query, [req.body.values], function(err, results, rows) {
+        //console.log(query.sql);
+        connection.end();
+        if(err) {
+            res.status(500).json({
+                result: 'error',
+                error: err
+            });
+        } else {
+            res.json({
+                result: 'success',
+                query: query.sql
+            });
+        }
+    });
+});
+
+module.exports = router;
